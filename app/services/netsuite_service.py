bb2cbc813483fca217ccb1c66694569faa1ce0dc
Tapendra <maayinstechnologies@gmail.com>
2025-09-19 14:31:59 -0500

    Remove sec

--- app/services/netsuite_service.py
+++ app/services/netsuite_service.py
@@ -14,12 +14,12 @@ def run_suite_ql_http_client(
     offset: Optional[str] = 0
 ) -> str:
     
-    nsAccountID = "627386"
-    consumerKey = "7d84f4f4c4cac7b7689facf5ab387bd896723ae7ce748bf5dbdb89e1f593fa55"
-    consumerSecret = "8b58da4ec2daa13b3f41729321fdfa080f5685a94f6b8273f7091f9fae70cca0"
-    token = "9026f8433cb32c15cb6bebd32f3e8115bc60522d7c12a257fbe4536e3bf5c511"
-    tokenSecret = "093f4afc4111ba1016d7788a34ab56f6451be7476e1fd93d2d3009ec9867436b"
-    base_url = "https://627386.suitetalk.api.netsuite.com/services/rest/query/v1/suiteql"
+    nsAccountID = "xxxx"
+    consumerKey = "xxx"
+    consumerSecret = "xxx"
+    token = "xxx"
+    tokenSecret = "xxx"
+    base_url = "https://xxx.suitetalk.api.netsuite.com/services/rest/query/v1/suiteql"
     Nonce = NetSuiteAuth._generateNonce(length=11)
     currentTime = NetSuiteAuth._generateTimestamp()
 
